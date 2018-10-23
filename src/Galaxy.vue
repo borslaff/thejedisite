c0e97086b71b4670f491aa45179d78e45dde6ebc
Boris Vellekoop <boris@studyportals.com>
2018-10-23 10:30:12 +0200

    A new hope

---
+++ src/Galaxy.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>Sterretjes {{ stars }}</p>
+        <button @click="count"> count dem stars</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            stars: 'a lot...',
+        }
+    },
+    methods: {
+        count(){
+            this.stars = 'To much';
+        }
+    }
+}
+</script>
